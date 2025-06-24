effd7f767546efe5ce1629faaa0c15b3113b8687
Victor Schweitz <victorschweitz@hotmail.com>
2025-06-25 00:12:03 +0200

    Set up the single view walkthrough page for Bloons. Rest of the games still missing.

---
+++ src/js/games.js
@@ -0,0 +1,517 @@
+export const games = [
+    // Bloons
+    {
+      slug: "bloons",
+      levels: [
+        {
+          levelName: "1. Pop Me",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/pop-me",
+        },
+        {
+          levelName: "2. Pop Me 2",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/pop-me-two",
+        },
+        {
+          levelName: "3. Surprise",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/surprise",
+        },
+        {
+          levelName: "4. Triangles",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/triangles",
+        },
+        {
+          levelName: "5. Starburst",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/starburst",
+        },
+        {
+          levelName: "6. The Iron Curtain",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/the-iron-curtain",
+        },
+        {
+          levelName: "7. Dart from above",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/dart-from-above",
+        },
+        {
+          levelName: "8. Holy Moley!",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/holy-moley",
+        },
+        {
+          levelName: "9. Boing",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/boing",
+        },
+        {
+          levelName: "10. Parallel",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/parallel",
+        },
+        {
+          levelName: "11. Tack-tile",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/tack-tile",
+        },
+        {
+          levelName: "12. Arc",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/bloons-level-one",
+        },
+        {
+          levelName: "13. Reservoir",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/reservoir",
+        },
+        {
+          levelName: "14. Maze",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/maze",
+        },
+        {
+          levelName: "15. One in a hundred",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/one-in-a-hundred",
+        },
+        {
+          levelName: "16. Boomer",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/boomer",
+        },
+        {
+          levelName: "17. Shapes",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/shapes",
+        },
+        {
+          levelName: "18. Chain Reaction",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/chain-reaction",
+        },
+        {
+          levelName: "19. Light The Fuse",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/light-the-fuse",
+        },
+        {
+          levelName: "20. Loopy",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/loopy",
+        },
+        {
+          levelName: "21. Throwback",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/throwback",
+        },
+        {
+          levelName: "22. Round-a-bout",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/round-a-bout",
+        },
+        {
+          levelName: "23. Ouchie",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/ouchie",
+        },
+        {
+          levelName: "24. Monsta",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/monsta",
+        },
+        {
+          levelName: "25. Demolition",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/demolition",
+        },
+        {
+          levelName: "26. 2 Paths",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/two-paths",
+        },
+        {
+          levelName: "27. Bouncy",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/bouncy",
+        },
+        {
+          levelName: "28. Big Ball",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/big-ball",
+        },
+        {
+          levelName: "29. Air Raid",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/air-raid",
+        },
+        {
+          levelName: "30. Ice Bounce",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/ice-bounce",
+        },
+        {
+          levelName: "31. The Cell",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/the-cell",
+        },
+        {
+          levelName: "32. Backwards Glance",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/backwards-glance",
+        },
+        {
+          levelName: "33. Danger!",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/danger",
+        },
+        {
+          levelName: "34. Igloos",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/igloos",
+        },
+        {
+          levelName: "35. Fuggetaboudit",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/fuggetaboudit",
+        },
+        {
+          levelName: "36. Chalice",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/chalice",
+        },
+        {
+          levelName: "37. Pacatak",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/pacatak",
+        },
+        {
+          levelName: "38. Bouncebomb",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/bouncebomb",
+        },
+        {
+          levelName: "39. Lockdown",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/lockdown",
+        },
+        {
+          levelName: "40. Ice Cave",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/ice-cave",
+        },
+        {
+          levelName: "41. Myriad",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/myriad",
+        },
+        {
+          levelName: "42. Chilly Chips",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/chilly-chips",
+        },
+        {
+          levelName: "43. D in the D",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/d-in-the-d",
+        },
+        {
+          levelName: "44. Showertime",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/showertime",
+        },
+
+        {
+          levelName: "45. Not So Hot",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/not-so-hot",
+        },
+        {
+          levelName: "46. Ears",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/ears",
+        },
+        {
+          levelName: "47. Bloon of Damacles",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/bloon-of-damacles",
+        },
+        {
+          levelName: "48. The Matrix",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/the-matrix",
+        },
+        {
+          levelName: "49. Wrong Side",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/wrong-side",
+        },
+        {
+          levelName: "50. The End",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/the-end",
+        },
+      ],
+    },
+    
+    // More Bloons
+    {
+      slug: "more-bloons",
+      levels: [
+        {
+          levelName: "1. Pop Me",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/pop-me",
+        },
+        {
+          levelName: "2. Pop Me 2",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/pop-me-two",
+        },
+        {
+          levelName: "3. Surprise",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/surprise",
+        },
+        {
+          levelName: "4. Triangles",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/triangles",
+        },
+        {
+          levelName: "5. Starburst",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/starburst",
+        },
+        {
+          levelName: "6. The Iron Curtain",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/the-iron-curtain",
+        },
+        {
+          levelName: "7. Dart from above",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/dart-from-above",
+        },
+        {
+          levelName: "8. Holy Moley!",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/holy-moley",
+        },
+        {
+          levelName: "9. Boing",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/boing",
+        },
+        {
+          levelName: "10. Parallel",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/parallel",
+        },
+        {
+          levelName: "11. Tack-tile",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/tack-tile",
+        },
+        {
+          levelName: "12. Arc",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/bloons-level-one",
+        },
+        {
+          levelName: "13. Reservoir",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/reservoir",
+        },
+        {
+          levelName: "14. Maze",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/maze",
+        },
+        {
+          levelName: "15. One in a hundred",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/one-in-a-hundred",
+        },
+        {
+          levelName: "16. Boomer",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/boomer",
+        },
+        {
+          levelName: "17. Shapes",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/shapes",
+        },
+        {
+          levelName: "18. Chain Reaction",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/chain-reaction",
+        },
+        {
+          levelName: "19. Light The Fuse",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/light-the-fuse",
+        },
+        {
+          levelName: "20. Loopy",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/loopy",
+        },
+        {
+          levelName: "21. Throwback",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/throwback",
+        },
+        {
+          levelName: "22. Round-a-bout",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/round-a-bout",
+        },
+        {
+          levelName: "23. Ouchie",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/ouchie",
+        },
+        {
+          levelName: "24. Monsta",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/monsta",
+        },
+        {
+          levelName: "25. Demolition",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/demolition",
+        },
+        {
+          levelName: "26. 2 Paths",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/two-paths",
+        },
+        {
+          levelName: "27. Bouncy",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/bouncy",
+        },
+        {
+          levelName: "28. Big Ball",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/big-ball",
+        },
+        {
+          levelName: "29. Air Raid",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/air-raid",
+        },
+        {
+          levelName: "30. Ice Bounce",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/ice-bounce",
+        },
+        {
+          levelName: "31. The Cell",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/the-cell",
+        },
+        {
+          levelName: "32. Backwards Glance",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/backwards-glance",
+        },
+        {
+          levelName: "33. Danger!",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/danger",
+        },
+        {
+          levelName: "34. Igloos",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/igloos",
+        },
+        {
+          levelName: "35. Fuggetaboudit",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/fuggetaboudit",
+        },
+        {
+          levelName: "36. Chalice",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/chalice",
+        },
+        {
+          levelName: "37. Pacatak",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/pacatak",
+        },
+        {
+          levelName: "38. Bouncebomb",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/bouncebomb",
+        },
+        {
+          levelName: "39. Lockdown",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/lockdown",
+        },
+        {
+          levelName: "40. Ice Cave",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/ice-cave",
+        },
+        {
+          levelName: "41. Myriad",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/myriad",
+        },
+        {
+          levelName: "42. Chilly Chips",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/chilly-chips",
+        },
+        {
+          levelName: "43. D in the D",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/d-in-the-d",
+        },
+        {
+          levelName: "44. Showertime",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/showertime",
+        },
+
+        {
+          levelName: "45. Not So Hot",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/not-so-hot",
+        },
+        {
+          levelName: "46. Ears",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/ears",
+        },
+        {
+          levelName: "47. Bloon of Damacles",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/bloon-of-damacles",
+        },
+        {
+          levelName: "48. The Matrix",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/the-matrix",
+        },
+        {
+          levelName: "49. Wrong Side",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/wrong-side",
+        },
+        {
+          levelName: "50. The End",
+          levelImage: "/images/games/bloons.webp",
+          href: "/walkthroughs/bloons/the-end",
+        },
+      ],
+    },
+  ]
